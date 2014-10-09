19e8c914fc90b73b013bc85b5d90fad8bd5e9299
wasifzaman <zelvenstormrage@gmail.com>
2014-10-08 23:40:38 -0400

    alpha_progress
    advanced the table prototype

--- alpha_build/testing_range.py
+++ alpha_build/testing_range.py
@@ -74,7 +74,7 @@ frame = Frame(window)
 
 canvas = Canvas(frame, width=700, height=300)
 
-canvas.create_rectangle(50, 25, 150, 75, fill="lightblue", outline="lightblue", tag='rect_0', width=0)
+rect_1 = canvas.create_rectangle(50, 25, 150, 75, fill="lightblue", outline="lightblue", tag='rect_0', width=0)
 canvas.create_rectangle(150, 25, 250, 75, fill="lightblue", outline="lightblue", tag='rect_0', width=0)
 canvas.create_rectangle(250, 25, 350, 75, fill="lightblue", outline="lightblue", tag='rect_0', width=0)
 
@@ -87,6 +87,7 @@ canvas.create_line(50, 25, 50, 75)
 
 canvas.create_text(100, 50, text='text', tag='rect_0_text')
 
+canvas.move(rect_1, 50, 50)
 
 def select_row(event):
 	for rect in canvas.find_withtag('rect_0'):
@@ -123,38 +124,52 @@ class Cell_object:
 		#self.tag = str(self.row) + ',' + str(self.column)
 		#canvas.itemconfig(self.object_id, tags=(self.tag))
 
-	def delete_line(self, line):
+	def erase_line(self, line):
 		if line == 'left':
-			self.canvas.delete(self.left_line)
+			self.canvas.itemconfig(self.left_line, state=HIDDEN)
 		elif line == 'right':
-			self.canvas.delete(self.right_line)
+			self.canvas.itemconfig(self.right_line, state=HIDDEN)
 		elif line == 'top':
-			self.canvas.delete(self.top_line)
+			self.canvas.itemconfig(self.top_line, state=HIDDEN)
 		elif line == 'bottom':
-			self.canvas.delete(self.bottom_line)
+			self.canvas.itemconfig(self.bottom_line, state=HIDDEN)
+		else:
+			return
+
+	def draw_line(self, line):
+		if line == 'left':
+			self.canvas.itemconfig(self.left_line, state=NORMAL)
+		elif line == 'right':
+			self.canvas.itemconfig(self.right_line, state=NORMAL)
+		elif line == 'top':
+			self.canvas.itemconfig(self.top_line, state=NORMAL)
+		elif line == 'bottom':
+			self.canvas.itemconfig(self.bottom_line, state=NORMAL)
 		else:
 			return
 
 	pass
 
-cell = Cell_object(canvas, 350, 25, 450, 75, 0, 0)
-canvas.create_text(cell.center, text='text', tag='rect_0_text')
-
-#canvas.create_line(cell.left_line)
-#canvas.create_line(cell.top_line)
-#canvas.create_line(cell.bottom_line)
-#canvas.create_line(cell.right_line)
-
-canvas.itemconfig(cell.object_id, fill='lightgreen')
-
-string = "0;50,25;150,75"
-
-print(string.split(';'))
-
 frame.grid()
+'''
+	cell = Cell_object(canvas, 350, 25, 450, 75, 0, 0)
+	canvas.create_text(cell.center, text='text', tag='rect_0_text')
 
-#canvas.grid()
+	#canvas.create_line(cell.left_line)
+	#canvas.create_line(cell.top_line)
+	#canvas.create_line(cell.bottom_line)
+	#canvas.create_line(cell.right_line)
 
+	canvas.itemconfig(cell.object_id, fill='lightgreen')
+
+	string = "0;50,25;150,75"
+
+	print(string.split(';'))
+
+	
+
+	canvas.grid()
+'''
 
 class Table:
 
@@ -171,25 +186,200 @@ class Table:
 			while row < num_rows:
 				self.cells[(column, row)] = Cell_object(self.canvas, x, y, x + 100, y + 25, row, column)
 				y += 25
-
-				#self.cells[(column, row)].delete_line('top')
-				#self.canvas.create_line(self.cells[(column, row)].left_line)
-				#self.canvas.create_line(self.cells[(column, row)].right_line)
-				#self.canvas.create_line(self.cells[(column, row)].bottom_line)
 				
 				row += 1
 			
 			x += 100
 			y = 5	
 
+
 			column += 1
 			row = 0
+	
+	def erase_line(self, cell, line):
+
+		def erase_single_line(line):
+
+			if cell not in self.cells: return
+
+			if line == 'left':
+				cell_left = (cell[0] - 1, cell[1])
+				if cell_left in self.cells:
+					self.cells[cell_left].erase_line('right')
+				self.cells[cell].erase_line('left')
+			elif line == 'right':
+				cell_right = (cell[0] + 1, cell[1])
+				if cell_right in self.cells:
+					self.cells[cell_right].erase_line('left')
+				self.cells[cell].erase_line('right')
+			elif line == 'top':
+				cell_right = (cell[0], cell[1] - 1)
+				if cell_right in self.cells:
+					self.cells[cell_right].erase_line('bottom')
+				self.cells[cell].erase_line('top')
+			elif line == 'bottom':
+				cell_right = (cell[0], cell[1] + 1)
+				if cell_right in self.cells:
+					self.cells[cell_right].erase_line('top')
+				self.cells[cell].erase_line('bottom')
+			else:
+				return
+
+		if type(line) == list:
+
+			for single_line in line:
+				erase_single_line(single_line)
+		else:
+			erase_single_line(line)
+
+	def draw_line(self, cell, line):
+
+		def draw_single_line(line):
+
+			if cell not in self.cells: return
+
+			if line == 'left':
+				cell_left = (cell[0] - 1, cell[1])
+				if cell_left in self.cells:
+					self.cells[cell_left].draw_line('right')
+				self.cells[cell].draw_line('left')
+			elif line == 'right':
+				cell_right = (cell[0] + 1, cell[1])
+				if cell_right in self.cells:
+					self.cells[cell_right].draw_line('left')
+				self.cells[cell].draw_line('right')
+			elif line == 'top':
+				cell_right = (cell[0], cell[1] - 1)
+				if cell_right in self.cells:
+					self.cells[cell_right].draw_line('bottom')
+				self.cells[cell].draw_line('top')
+			elif line == 'bottom':
+				cell_right = (cell[0], cell[1] + 1)
+				if cell_right in self.cells:
+					self.cells[cell_right].draw_line('top')
+				self.cells[cell].draw_line('bottom')
+			else:
+				return
+
+		if type(line) == list:
+
+			for single_line in line:
+				draw_single_line(single_line)
+		else:
+			draw_single_line(line)
+
+	def add_row(self, after):
+
+		if after >= self.num_rows:
+			after = self.num_rows
+
+			x, y, row, column = 5, self.cells[(0, self.num_rows - 1)].p2y, after, 0
+
+			while column < self.num_columns:
+				self.cells[(column, row)] = Cell_object(self.canvas, x, y, x + 100, y + 25, row, column)
+
+				column += 1
+
+				x += 100
+
+		else:
+
+			x, y, row, column = 5, self.cells[(0, after)].p2y, self.num_rows - 1, 0
+
+			while row > after - 1:
+				while column < self.num_columns:
+
+					self.canvas.move(self.cells[(column, row)].object_id, 0, 25)
+
+					left_line_state = self.canvas.itemcget(self.cells[(column, row)].left_line, 'state')
+					right_line_state = self.canvas.itemcget(self.cells[(column, row)].right_line, 'state')
+					top_line_state = self.canvas.itemcget(self.cells[(column, row)].top_line, 'state')
+					bottom_line_state = self.canvas.itemcget(self.cells[(column, row)].bottom_line, 'state')
+
+					print(left_line_state)
+
+					self.canvas.delete(self.cells[(column, row)].left_line)
+					self.canvas.delete(self.cells[(column, row)].right_line)
+					self.canvas.delete(self.cells[(column, row)].top_line)
+					self.canvas.delete(self.cells[(column, row)].bottom_line)
+
+					self.cells[(column, row + 1)] = self.cells[(column, row)]
+					moved_cell = self.cells[(column, row + 1)]
+					moved_cell.p1y = moved_cell.p1y + 25
+					moved_cell.p2y = moved_cell.p2y + 25
+
+					moved_cell.left_line = self.canvas.create_line(moved_cell.p1x, moved_cell.p1y, moved_cell.p1x, moved_cell.p2y, state=left_line_state)
+					moved_cell.right_line = self.canvas.create_line(moved_cell.p2x, moved_cell.p1y, moved_cell.p2x, moved_cell.p2y)
+					moved_cell.top_line = self.canvas.create_line(moved_cell.p1x, moved_cell.p1y, moved_cell.p2x, moved_cell.p1y)
+					moved_cell.bottom_line = self.canvas.create_line(moved_cell.p1x, moved_cell.p2y, moved_cell.p2x, moved_cell.p2y)
+
+					column += 1
+
+				column = 0
+				row -= 1
+	
+		self.num_rows += 1
+	
+		self.canvas.config(height=self.num_rows * 25 + 5)
+
+	def add_column(self, after):
+
+		if after >= self.num_columns:
+			after = self.num_columns
+
+			x, y, row, column = self.cells[(self.num_columns - 1, 0)].p2x, 5, 0, after
+
+			while row < self.num_rows:
+				self.cells[(column, row)] = Cell_object(self.canvas, x, y, x + 100, y + 25, row, column)
+
+				row += 1
+
+				y += 25
+
+		else:
+
+			x, y, row, column = self.cells[(after, 0)].p2x, 5, 0, self.num_columns - 1
+
+			while column > after - 1:
+				while row < self.num_rows:
+
+					self.canvas.move(self.cells[(column, row)].object_id, 100, 0)
+					self.canvas.delete(self.cells[(column, row)].left_line)
+					self.canvas.delete(self.cells[(column, row)].right_line)
+					self.canvas.delete(self.cells[(column, row)].top_line)
+					self.canvas.delete(self.cells[(column, row)].bottom_line)
+
+					self.cells[(column + 1, row)] = self.cells[(column, row)]
+					moved_cell = self.cells[(column + 1, row)]
+					moved_cell.p1x = moved_cell.p1x + 100
+					moved_cell.p2x = moved_cell.p2x + 100
+
+					moved_cell.left_line = self.canvas.create_line(moved_cell.p1x, moved_cell.p1y, moved_cell.p1x, moved_cell.p2y)
+					moved_cell.right_line = self.canvas.create_line(moved_cell.p2x, moved_cell.p1y, moved_cell.p2x, moved_cell.p2y)
+					moved_cell.top_line = self.canvas.create_line(moved_cell.p1x, moved_cell.p1y, moved_cell.p2x, moved_cell.p1y)
+					moved_cell.bottom_line = self.canvas.create_line(moved_cell.p1x, moved_cell.p2y, moved_cell.p2x, moved_cell.p2y)
+
+					row += 1
+
+				column -= 1
+				row = 0
+	
+		self.num_columns += 1
+	
+		self.canvas.config(width=self.num_columns * 100 + 5)
+			
+
+
 
 	pass
 
 
 table = Table(frame, 5, 5)
 
+table.erase_line((0, 1), 'left')
+#table.draw_line((0, 0), 'left')
+#table.add_row(5)
+#table.add_column(5)
 
 window.mainloop()
 
